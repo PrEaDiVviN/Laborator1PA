92ce443c2239a7b037e28bf45cb9a2a28ec2e0a6
PrEaDiVviN <75678649+PrEaDiVviN@users.noreply.github.com>
2021-03-03 23:57:17 +0200

    Delete laboratorul2/Optional/doc directory

--- laboratorul2/Optional/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.company","l":"Destination"},{"p":"com.company","l":"DestinationType"},{"p":"com.company","l":"Factory"},{"p":"com.company","l":"Main"},{"p":"com.company","l":"Problem"},{"p":"com.company","l":"Solution"},{"p":"com.company","l":"Source"},{"p":"com.company","l":"Warehouse"}];updateSearchResults();
