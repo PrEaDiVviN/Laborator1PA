1f55e06902f3773b3a0113e13f419a6f490b7fc8
PrEaDiVviN <75678649+PrEaDiVviN@users.noreply.github.com>
2021-03-17 23:09:23 +0200

    Update Main.java

--- Laboratorul5/Compulsory/Main.java
+++ Laboratorul5/Compulsory/Main.java
@@ -19,13 +19,22 @@ public class Main {
     private void testAddAndList() {
         Catalog catalog = new Catalog("My catalog", "C:\\Users\\PrEaD\\Desktop\\Anul2_semestrul2\\PA");
         var imageCar = new Image("CarImage", "Big car", "C:\\Users\\PrEaD\\Pictures\\Saved Pictures\\bigcar.png", "A awesome monster truck.", "Andrew Carson", Date.valueOf("2014-05-23"), 1920, 1920);
-        var imageForest = new Image("ForestImage", "Forest with fog from mountain", "C:\\Users\\PrEaD\\Pictures\\Saved Pictures\\Forest.jpg", "A beautiful view of a forest with it's fox. Best for windows wallpaper", "Jaymantri", Date.valueOf("2012-03-15"), 1280, 720);
-        var movieCars = new Movie("CarMovie", "Fast & Furious 7", "C:\\Users\\PrEaD\\Pictures\\Saved Pictures\\fndf7.mp4", Duration.ofSeconds(8400), Date.valueOf("2015-04-03"), "1,516 miliarde $", 9.2f);
-        var movieAction = new Movie("ActionMovie", "Spider-Man 3", "C:\\Users\\PrEaD\\Pictures\\Saved Pictures\\spider.mp4", Duration.ofSeconds(9360), Date.valueOf("2007-04-16"), " 895 de milioane $", 8.6f);
-        var songFirst = new Song("BestSong", "SummerTime", "https://www.youtube.com/watch?v=25N1pdzvp4c&ab_channel=K-391", Duration.ofSeconds(282), Date.valueOf("2014-09-05"), "K-391");
-        var songSecond = new Song("GoodSong", "Better Days", "https://www.youtube.com/watch?v=VGP6JP_hAPM&ab_channel=AirwaveMusicTV", Duration.ofSeconds(188), Date.valueOf("2019-11-18"), "Arman Cekin & Faydee");
-        var bookFantasy = new Book("FantasyBook", "Harry Potter si Piatra Filozofala", "https://docs.google.com/viewer?a=v&pid=sites&srcid=ZGVmYXVsdGRvbWFpbnxyb21hbmVob3Jyb3J8Z3g6NDI4MDI2MWQ4MjZjZWM4MA", 152, 9.6f, Date.valueOf("1997-06-30"), "J. K. Rowling");
-        var bookMotivational = new Book("MotivationalBook", "Arta subtila a nepasarii", "https://msbook.pro/motivatisisucces/1154-arta-subtil-a-nepsrii-de-mark-manson-descarc-online-gratis-pdf.html", 241, 9.1f, Date.valueOf("2016-09-13"), "Mark Manson");
+        var imageForest = new Image("ForestImage", "Forest with fog from mountain", "C:\\Users\\PrEaD\\Pictures\\Saved Pictures\\Forest.jpg",
+                "A beautiful view of a forest with it's fox. Best for windows wallpaper", "Jaymantri", Date.valueOf("2012-03-15"), 1280, 720);
+        var movieCars = new Movie("CarMovie", "Fast & Furious 7", "C:\\Users\\PrEaD\\Pictures\\Saved Pictures\\fndf7.mp4", Duration.ofSeconds(8400),
+                Date.valueOf("2015-04-03"), "1,516 miliarde $", 9.2f);
+        var movieAction = new Movie("ActionMovie", "Spider-Man 3", "C:\\Users\\PrEaD\\Pictures\\Saved Pictures\\spider.mp4", Duration.ofSeconds(9360),
+                Date.valueOf("2007-04-16"), " 895 de milioane $", 8.6f);
+        var songFirst = new Song("BestSong", "SummerTime", "https://www.youtube.com/watch?v=25N1pdzvp4c&ab_channel=K-391", Duration.ofSeconds(282),
+                Date.valueOf("2014-09-05"), "K-391");
+        var songSecond = new Song("GoodSong", "Better Days", "https://www.youtube.com/watch?v=VGP6JP_hAPM&ab_channel=AirwaveMusicTV", Duration.ofSeconds(188),
+                Date.valueOf("2019-11-18"), "Arman Cekin & Faydee");
+        var bookFantasy = new Book("FantasyBook", "Harry Potter si Piatra Filozofala",
+                "https://docs.google.com/viewer?a=v&pid=sites&srcid=ZGVmYXVsdGRvbWFpbnxyb21hbmVob3Jyb3J8Z3g6NDI4MDI2MWQ4MjZjZWM4MA", 152, 9.6f,
+                Date.valueOf("1997-06-30"), "J. K. Rowling");
+        var bookMotivational = new Book("MotivationalBook", "Arta subtila a nepasarii",
+                "https://msbook.pro/motivatisisucces/1154-arta-subtil-a-nepsrii-de-mark-manson-descarc-online-gratis-pdf.html", 241, 9.1f,
+                Date.valueOf("2016-09-13"), "Mark Manson");
         catalog.addItem(imageCar);
         catalog.addItem(imageForest);
         catalog.addItem(movieCars);
@@ -39,10 +48,15 @@ public class Main {
 
     private void testPlay() {
         Catalog catalog = new Catalog("My catalog", "C:\\Users\\PrEaD\\Desktop\\Anul2_semestrul2\\PA\\Laboratorul5\\src\\com\\company");
-        var imageForest = new Image("ForestImage", "Forest with fog from mountain", "C:\\Users\\PrEaD\\Pictures\\Saved Pictures\\Forest.jpg", "A beautiful view of a forest with it's fox. Best for windows wallpaper", "Jaymantri", Date.valueOf("2012-03-15"), 1280, 720);
-        var movieAction = new Movie("ActionMovie", "Spider-Man 3", "C:\\Users\\PrEaD\\Pictures\\Saved Pictures\\spider.mp4", Duration.ofSeconds(9360), Date.valueOf("2007-04-16"), " 895 de milioane $", 8.6f);
-        var songSecond = new Song("GoodSong", "Better Days", "https://www.youtube.com/watch?v=VGP6JP_hAPM&ab_channel=AirwaveMusicTV", Duration.ofSeconds(188), Date.valueOf("2019-11-18"), "Arman Cekin & Faydee");
-        var bookFantasy = new Book("FantasyBook", "Harry Potter si Piatra Filozofala", "https://docs.google.com/viewer?a=v&pid=sites&srcid=ZGVmYXVsdGRvbWFpbnxyb21hbmVob3Jyb3J8Z3g6NDI4MDI2MWQ4MjZjZWM4MA", 152, 9.6f, Date.valueOf("1997-06-30"), "J. K. Rowling");
+        var imageForest = new Image("ForestImage", "Forest with fog from mountain", "C:\\Users\\PrEaD\\Pictures\\Saved Pictures\\Forest.jpg",
+                "A beautiful view of a forest with it's fox. Best for windows wallpaper", "Jaymantri", Date.valueOf("2012-03-15"), 1280, 720);
+        var movieAction = new Movie("ActionMovie", "Spider-Man 3", "C:\\Users\\PrEaD\\Pictures\\Saved Pictures\\spider.mp4", Duration.ofSeconds(9360),
+                Date.valueOf("2007-04-16"), " 895 de milioane $", 8.6f);
+        var songSecond = new Song("GoodSong", "Better Days", "https://www.youtube.com/watch?v=VGP6JP_hAPM&ab_channel=AirwaveMusicTV", Duration.ofSeconds(188),
+                Date.valueOf("2019-11-18"), "Arman Cekin & Faydee");
+        var bookFantasy = new Book("FantasyBook", "Harry Potter si Piatra Filozofala",
+                "https://docs.google.com/viewer?a=v&pid=sites&srcid=ZGVmYXVsdGRvbWFpbnxyb21hbmVob3Jyb3J8Z3g6NDI4MDI2MWQ4MjZjZWM4MA", 152, 9.6f,
+                Date.valueOf("1997-06-30"), "J. K. Rowling");
 
         catalog.addItem(imageForest);
         catalog.addItem(movieAction);
@@ -60,10 +74,15 @@ public class Main {
 
     private void testSave() {
         Catalog catalog = new Catalog("My catalog", "C:\\Users\\PrEaD\\Desktop\\Anul2_semestrul2\\PA\\Laboratorul5\\src\\com\\company\\Catalog.ser");
-        var imageForest = new Image("ForestImage", "Forest with fog from mountain", "C:\\Users\\PrEaD\\Pictures\\Saved Pictures\\Forest.jpg", "A beautiful view of a forest with it's fox. Best for windows wallpaper", "Jaymantri", Date.valueOf("2012-03-15"), 1280, 720);
-        var movieAction = new Movie("ActionMovie", "Spider-Man 3", "C:\\Users\\PrEaD\\Pictures\\Saved Pictures\\spider.mp4", Duration.ofSeconds(9360), Date.valueOf("2007-04-16"), " 895 de milioane $", 8.6f);
-        var songSecond = new Song("GoodSong", "Better Days", "https://www.youtube.com/watch?v=VGP6JP_hAPM&ab_channel=AirwaveMusicTV", Duration.ofSeconds(188), Date.valueOf("2019-11-18"), "Arman Cekin & Faydee");
-        var bookFantasy = new Book("FantasyBook", "Harry Potter si Piatra Filozofala", "https://docs.google.com/viewer?a=v&pid=sites&srcid=ZGVmYXVsdGRvbWFpbnxyb21hbmVob3Jyb3J8Z3g6NDI4MDI2MWQ4MjZjZWM4MA", 152, 9.6f, Date.valueOf("1997-06-30"), "J. K. Rowling");
+        var imageForest = new Image("ForestImage", "Forest with fog from mountain", "C:\\Users\\PrEaD\\Pictures\\Saved Pictures\\Forest.jpg",
+                "A beautiful view of a forest with it's fox. Best for windows wallpaper", "Jaymantri", Date.valueOf("2012-03-15"), 1280, 720);
+        var movieAction = new Movie("ActionMovie", "Spider-Man 3", "C:\\Users\\PrEaD\\Pictures\\Saved Pictures\\spider.mp4",
+                Duration.ofSeconds(9360), Date.valueOf("2007-04-16"), " 895 de milioane $", 8.6f);
+        var songSecond = new Song("GoodSong", "Better Days", "https://www.youtube.com/watch?v=VGP6JP_hAPM&ab_channel=AirwaveMusicTV",
+                Duration.ofSeconds(188), Date.valueOf("2019-11-18"), "Arman Cekin & Faydee");
+        var bookFantasy = new Book("FantasyBook", "Harry Potter si Piatra Filozofala",
+                "https://docs.google.com/viewer?a=v&pid=sites&srcid=ZGVmYXVsdGRvbWFpbnxyb21hbmVob3Jyb3J8Z3g6NDI4MDI2MWQ4MjZjZWM4MA", 152, 9.6f,
+                Date.valueOf("1997-06-30"), "J. K. Rowling");
 
         catalog.addItem(imageForest);
         catalog.addItem(movieAction);
@@ -85,7 +104,6 @@ public class Main {
         catch (InvalidCatalogException | IOException | ClassNotFoundException e) {
             System.out.println(e.getMessage());
         }
- 
+
     }
 }
- 
