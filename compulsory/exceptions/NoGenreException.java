0c208d6a83ff1c0cede7435b2754b62c6b44cc45
PrEaDiVviN <75678649+PrEaDiVviN@users.noreply.github.com>
2021-04-16 00:16:42 +0300

    Delete compulsory directory

--- compulsory/exceptions/NoGenreException.java
+++
@@ -1,7 +0,0 @@
-package compulsory.exceptions;
-
-public class NoGenreException extends Exception{
-    public NoGenreException() {
-        super("Exception: no genre was set for the current operation");
-    }
-}
--- compulsory/sqlscript/databases.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE linkmovtogen;
-DROP TABLE movies;
-DROP TABLE genres;
-
-CREATE TABLE movies(id int PRIMARY KEY, titlu VARCHAR(30) NOT NULL UNIQUE, release_date DATE NOT NULL, duration TIME NOT NULL, score int);
-CREATE TABLE genres(id int PRIMARY KEY, titlu VARCHAR(30) NOT NULL UNIQUE);
-CREATE TABLE linkmovtogen(id_mov int, id_gen int, FOREIGN KEY (id_mov) REFERENCES movies(id), FOREIGN KEY (id_gen) REFERENCES genres(id));
