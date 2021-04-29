c28576b6233c889a995973610e1e4d9dd321c483
PrEaDiVviN <75678649+PrEaDiVviN@users.noreply.github.com>
2021-04-30 01:01:52 +0300

    Update ClientHandler.java

--- Laboratorul10/Client/src/main/java/handlers/ClientHandler.java
+++ Laboratorul10/Client/src/main/java/handlers/ClientHandler.java
@@ -54,7 +54,11 @@ public class ClientHandler {
             else if(reply.contains("login"))
                 this.handleLogin(output,input);
             else if(reply.contains("register"))
-                this.handleRegister();
+                this.handleRegister(output,input);
+            else if(reply.contains("send"))
+            	this.handleSent(output,input);
+            else if(reply.contains("read"))
+            	this.handleRead(output,input);
             else {
                 try {
                     System.out.println(input.readLine());
@@ -66,65 +70,121 @@ public class ClientHandler {
     }
 
     private void handleLogin(PrintWriter output, BufferedReader input ) {
-        System.out.println("LOGIN--->");
-
+    	//citim intructiunea de la server
         try {
         	String answer = input.readLine();
             System.out.println(answer);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("LOGIN--->1");
-        //citim  comanda de la tastatura
+        //citim  numele de utilizator de la tastatura
         Scanner keyboard = new Scanner(System.in);
         String reply = keyboard.nextLine();
-        //trimitem comanda la server
+        //trimitem numele de utilizator la server
         output.println(reply);
+        //citim intruciunea de la la server
+        String serverAnswer = null;
         try {
-            System.out.println(input.readLine());
+        	serverAnswer = input.readLine();
+            System.out.println(serverAnswer);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        reply = keyboard.nextLine();
-        output.println(reply);
-        try {
-            System.out.println(input.readLine());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        //verificam daca raspunsul serverului este o eroare
+        if(!serverAnswer.startsWith("Error")) {
+        	//citim parola de la tastatura
+	        reply = keyboard.nextLine();
+	        //trimitem parola catre server
+	        output.println(reply);
+	        try {
+	        	//citim raspunsul de la server
+	            System.out.println(input.readLine());
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+    	}
     }
-    private void handleRegister() {
-        PrintWriter output = null;
-        try {
-            output = new PrintWriter(clientTcpConnection.getCommunicationSocket().getOutputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        BufferedReader input = null;
-        try {
-            input = new BufferedReader(new InputStreamReader(clientTcpConnection.getCommunicationSocket().getInputStream()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private void handleRegister(PrintWriter output, BufferedReader input ) {
+        //citim intructiunea de la server
         try {
             System.out.println(input.readLine());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //citim  comanda de la tastatura
+        //citim  numele de utilizator de latastatura
         Scanner keyboard = new Scanner(System.in);
         String reply = keyboard.nextLine();
-        //trimitem comanda la server
-        output.print(reply);
+        //trimitem numele de utilizator la server
+        output.println(reply);
+        //primim raspunsul de la server si il afisam pe ercan
+        String answer = null;
         try {
-            System.out.println(input.readLine());
+        	answer = input.readLine();
+            System.out.println(answer);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        reply = keyboard.nextLine();
-        output.print(reply);
+        //verificam daca am primit eroare
+	    if(!answer.startsWith("Error")) {
+        	//citim parola de la tastatura
+      		reply = keyboard.nextLine();
+      		//trimitem parola catre server
+        	output.println(reply);
+        	//primim raspunsul de la server
+        	try {
+            	System.out.println(input.readLine());
+        	} catch (IOException e) {
+            	e.printStackTrace();
+        	}
+    	}
+    }
+
+      public void handleSent(PrintWriter output, BufferedReader input) {
+        //citim prima instructiune de la server
+        String instructiune = null;
         try {
-            System.out.println(input.readLine());
+            instructiune = input.readLine();
+            System.out.println(instructiune);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //citim username-ul de la tastatura 
+        Scanner keyboard = new Scanner(System.in);
+        String username = keyboard.nextLine();
+
+        //trimitem username-ul catre server
+        output.println(username);
+        //citim reply-ul serverului legat de user
+        String reply = null;
+        try {
+            reply = input.readLine();
+            System.out.println(reply);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(!reply.startsWith("Error")) {
+        	//citim de la tastatura mesajul ce trebuie trimis
+        	System.out.println("Suntem blocati de tastatura?");
+        	String message = keyboard.nextLine();
+        	//trimitem mesajul catre server
+        	output.println(message);
+        	//citim de la server raspunsul
+        	try {
+        	System.out.println(input.readLine());	
+        	}
+        	catch(Exception e) {
+        		e.printStackTrace();
+        	}
+        } 
+        
+    }
+
+    private void handleRead(PrintWriter output, BufferedReader input) {
+    	//citim  mesajele de la server si le afisam pe ecran
+        String mesaje = null;
+        try {
+            mesaje = input.readLine();
+            System.out.println(mesaje.replace('&','\n'));
         } catch (IOException e) {
             e.printStackTrace();
         }
